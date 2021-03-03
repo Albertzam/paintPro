def9558dc8f772d106c0ef78d1ffc07fe03e14b2
Albertzam <47655032+Albertzam@users.noreply.github.com>
2021-03-03 13:07:50 -0700

    linea v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('canvas', { static: false }) canvas: any;
   public width = 800;
   public height = 800;
+  imageData;
   private cx: CanvasRenderingContext2D;
   private points: any[] = [];
   private x: any;
@@ -54,6 +55,7 @@ export class AppComponent implements OnInit, AfterViewInit {
           endX: this.fx,
           endY: this.fy
         });
+        console.log(this.points);
         this.isDrawin = false;
         this.isAvailable = false;
       }
@@ -61,25 +63,95 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
   }
 
-
   private drawLine() {
+    this.cx.fillStyle = "white";
     this.cx.fillRect(0, 0, this.width, this.height);
-    this.cx.lineWidth = 2;
-  
+    /* 
+      for (var i = 0; i < this.points.length; ++i) {
+        this.cx.beginPath();
+        this.cx.moveTo(this.points[i].startX, this.points[i].startY);
+        this.cx.lineTo(this.points[i].endX,this.points[i].endY);
+        this.cx.stroke();
+      }
+      
+      if (this.isDrawin) {
+        this.cx.beginPath();
+        this.cx.moveTo(this.x, this.y);
+        this.cx.lineTo(this.fx, this.fy);
+        this.cx.stroke();
+      }  */
     for (var i = 0; i < this.points.length; ++i) {
-      this.cx.beginPath();
-      this.cx.moveTo(this.points[i].startX, this.points[i].startY);
-      this.cx.lineTo(this.points[i].endX,this.points[i].endY);
-      this.cx.stroke();
+      this.pintar(this.points[i].startX, this.points[i].startY, this.points[i].endX, this.points[i].endY);
     }
-    
+
     if (this.isDrawin) {
-      this.cx.beginPath();
-      this.cx.moveTo(this.x, this.y);
-      this.cx.lineTo(this.fx, this.fy);
-      this.cx.stroke();
+      this.pintar(this.x, this.y, this.fx, this.fy);
     }
+
   }
+  private setPixel(x, y) {
+    var p = this.cx.createImageData(1, 1);
+    p.data[0] = 0;
+    p.data[1] = 0;
+    p.data[2] = 0;
+    p.data[3] = 255;
+    this.cx.putImageData(p, x, y);
+  }
+
+  private pintar(s1, h1, s2, h2) {
+    this.cx.fillStyle = "black";
+    var x1 = Math.round(s1);
+    var y1 = Math.round(h1);
+    var x2 = Math.round(s2);
+    var y2 = Math.round(h2);
+    const dx = Math.abs(x2 - x1);
+    const sx = x1 < x2 ? 1 : -1;
+    const dy = Math.abs(y2 - y1);
+    const sy = y1 < y2 ? 1 : -1;
+    var error, len, rev, count = dx;
+    this.cx.beginPath();
+
+    if (dx > dy) {
+      error = dx / 2;
+      rev = x1 > x2 ? 1 : 0;
+      if (dy > 1) {
+        error = 0;
+        count = dy - 1;
+        do {
+          len = error / dy + 2 | 0;
+          this.cx.rect(x1 - len * rev, y1, len, 1);
+          x1 += len * sx;
+          y1 += sy;
+          error -= len * dy - dx;
+        } while (count--);
+      }
+      if (error > 0) { this.cx.rect(x1, y2, x2 - x1, 1) }
+    } else if (dx < dy) {
+      error = dy / 2;
+      rev = y1 > y2 ? 1 : 0;
+      if (dx > 1) {
+        error = 0;
+        count--;
+        do {
+          len = error / dx + 2 | 0;
+          this.cx.rect(x1, y1 - len * rev, 1, len);
+          y1 += len * sy;
+          x1 += sx;
+          error -= len * dx - dy;
+        } while (count--);
+      }
+      if (error > 0) { this.cx.rect(x2, y1, 1, y2 - y1) }
+    } else {
+      do {
+        this.cx.rect(x1, y1, 1, 1);
+        x1 += sx;
+        y1 += sy;
+      } while (count--);
+    }
+    this.cx.fill();
+
+  }
+
 
   ngOnInit(): void {
   }
@@ -93,15 +165,16 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.bounds = canvasEl.getBoundingClientRect();
 
     this.cx = canvasEl.getContext('2d');
+
     canvasEl.width = this.width;
     canvasEl.height = this.height;
 
     this.canvas.lineWidth = 3;
     this.cx.lineCap = 'round';
-    this.cx.strokeStyle = "#fff";
+    this.cx.strokeStyle = "#000";
     this.drawLine();
   }
-  
+
   public clearCanvas() {
     this.points = [];
     this.cx.clearRect(0, 0, this.width, this.height);
